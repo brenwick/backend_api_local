43c82ee9c39ef97f9721fe408de480bfe6ceedef
brenwick <46047488+brenwick@users.noreply.github.com>
2020-10-21 19:13:20 -0700

    debug

--- api/string.js
+++ api/string.js
@@ -1,50 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-
-
 // @route    POST api/profile
 // @desc     Test route
-// @access   Public 
+// @access   Public
 router.get('/', (req, res) => {
-    console.log('\n-----Headers-----\n');
-    console.log(req.headers);
-    console.log('\n------Body-------\n');
-    console.log(req.body);
-    console.log('\n------Status-----\n');
-    console.log(res.statusCode);
+  console.log('\n-----Headers-----\n');
+  console.log(req.headers);
+  console.log('\n------Body-------\n');
+  console.log(req.body);
+  console.log('\n------Status-----\n');
+  console.log(res.statusCode);
 
-    
-    res.send({
-        "message" : "Successful request from /api/string"
-    })
-})
+  res.send({
+    message: 'Successful request from /api/string',
+  });
+});
 
 // @route    POST api/profile
 // @desc     Processes JSON test data
-// @access   Public 
+// @access   Public
 router.post('/', (req, res) => {
-    
-    console.log('\n-----Headers-----\n');
-    console.log(req.headers);
-    console.log('\n------Body-------\n');
-    console.log(req.body);
-    console.log('\n------Status-----\n');
-    console.log(res.statusCode);
+  console.log('\n-----Headers-----\n');
+  console.log(req.headers);
+  console.log('\n------Body-------\n');
+  console.log(req.body);
+  console.log('\n------Status-----\n');
+  console.log(res.statusCode);
 
-    const str = req.body.string;
+  const str = req.body.string;
 
-    if(str) {
-        res.send({
-            "message" : "Here is your string: " + str
-        });
-    }
-    else{
-        res.send({
-            "message" : "error, please send a string in JSON format"
-        })
-    }
+  if (str) {
+    res.send({
+      message: 'Here is your string: ' + str,
+    });
+  } else {
+    res.send({
+      message: req.body,
+    });
+  }
 });
-
 
 module.exports = router;
